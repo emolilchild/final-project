78dc4ec7e842bc6e552164eacf574b50bd2b9a78
emolilchild <andreqtqng@gmail.com>
2024-04-21 11:32:14 +0100

    1

--- SeatingPlan.py
+++ SeatingPlan.py
@@ -3,21 +3,29 @@ class SeatingPlan:
         #create a dictionary that contains and represents the layout and seats of the plane
         self.seats = {}
         self.rows = 80
-        self.column = ["A", "B", "C", "D", "E", "F"]
+        self.column = ["A", "B", "C", "X", "D", "E", "F"]
         #iterate over the number of rows in the airplane
         for row in range(1, self.rows+1):
             #for each row iterate over each column in the airplane
             for col in self.column:
-                #skip over the aisle seats where are not available for booking
-                if col != "C" and col != "D":
-                    #mark the bookable seats as "F"
-                    #the keys of the dictionary will be tuples consisting of rows and columns respectively
-                    self.seats[(row, col)] = "F"
-        #get the booking status of a specific seat
-        def get_seat_status(self, seat):
-            #identify which row the seat selected is in
-            #slice the string so that we only get the number in front (row number) and not column
-            row = int(seat[:-1])
-            #identidy which column the seat selected is in by applying the same concept
-            col = seat[-1]
-            return self.seats.get((row,col))
+                #mark the bookable seats as "F"
+                #the keys of the dictionary will be tuples consisting of rows and columns respectively
+                self.seats[(row, col)] = "F"
+            #mark the aisle with "X"
+            for row in range(1, self.rows+1):
+                self.seats[(row, "X")] = "X"
+            #mark the storage spaces with "S"
+            for col in ["D", "E", "F"]:
+                #only space 77DEF and 78DEF are storage areas
+                self.seats[(77, col)] = "S"
+                self.seats[(78, col)] = "S"
+    def display(self):
+        #tell the user the status of the seat selected
+        #iterate through the columns to get the status of each seat
+        for row in range(1, self.rows +1):
+            row_display = f"Row {row}"
+            for col in self.column:
+                #get the seat status
+                seat_status = self.seats.get((row, col), "")
+                row_display += f"{seat_status}"
+            print(row_display)
