bc7f2b37fbfa38ec19b0a62875e97cebe921bd7f
emolilchild <andreqtqng@gmail.com>
2024-04-21 11:46:04 +0100

    2

--- SeatAvailability.py
+++ SeatAvailability.py
@@ -1,4 +1,17 @@
 class SeatAvailability:
     #initialise the seating plan
-    def __init__(self):
+    def __init__(self, seating_plan):
         self.seating_plan = seating_plan
+    def check(self, seat):
+        #check if a seat is available
+        #get the row using slicing of a string
+        row = int(seat[:-1])
+        #get column using negative indexing
+        col = seat[-1]
+        #get the status of the seat
+        seat_status = self.seating_plan.seats.get((row, col))
+        if seat_status == "F":
+            return True
+        else:
+            return False
+        
--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from SeatAvailability import SeatAvailability
 from FreeSeat import FreeSeat
 from ExitProgram import ExitProgram
 from SeatingPlan import SeatingPlan
+
+def main_menu():
+    seating_plan = SeatingPlan()
+    seat_availability = SeatAvailability(seating_plan)
